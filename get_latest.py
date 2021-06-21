cf6916b6b863f09b13cb6458851366efefcd73c8
Eric Werner <ewerybody+github@gmail.com>
2021-06-21 22:45:01 +0200

    Create get_latest.py
    local test script

---
+++ get_latest.py
@@ -0,0 +1,28 @@
+import os
+import blupd8
+
+HOME = r'C:\Users\eric\io\tools\blender'
+
+
+def main():
+    releases = blupd8.get_releases()
+    have = have_versions()
+
+    releases[blupd8.PROJECT]
+    latest = sorted(releases[blupd8.PROJECT])[-1]
+    version_name = blupd8.PROJECT + '-' + latest
+
+    if version_name not in have:
+        blupd8.download(latest, HOME)
+
+
+def have_versions():
+    have = []
+    for item in os.scandir(HOME):
+        if item.is_dir() and item.name.startswith(blupd8.PROJECT):
+            have.append(item.name)
+    return have
+
+
+if __name__ == '__main__':
+    main()
