a89581b7d134100d3d7039ae49c8fc4f4fbfac77
Eric Werner <ewerybody+github@gmail.com>
2021-06-27 14:30:25 +0200

    Update get_latest.py
    starting blender when already available :)

--- get_latest.py
+++ get_latest.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+
 import blupd8
 
 HOME = r'C:\Users\eric\io\tools\blender'
@@ -15,14 +17,19 @@ def main():
     if version_name not in have:
         blupd8.download(latest, HOME, 'windows', 'zip')
     else:
-        latest
+        executable_path = os.path.join(have[version_name], blupd8.PROJECT + '.exe')
+        if not os.path.isfile(executable_path):
+            raise FileNotFoundError('No blender.exe here! %s' % executable_path)
+
+        pid = subprocess.Popen([executable_path])
+        print('started blender! pid: %s (%s)' % (pid, executable_path))
 
 
 def have_versions():
-    have = []
+    have = {}
     for item in os.scandir(HOME):
         if item.is_dir() and item.name.startswith(blupd8.PROJECT):
-            have.append(item.name)
+            have[item.name] = item.path
     return have
 
 
